e3278972785580244e527c68187d497ce3de298d
Matteo Cristiano <slb6113@gmail.com>
2025-05-02 23:25:02 +0200

    Refactor gh-runner-start.sh script for improved clarity and error handling

--- script/bash/ubuntu-24.04/gh-runner-start.sh
+++ script/bash/ubuntu-24.04/gh-runner-start.sh
@@ -1,25 +1,25 @@
+#!/bin/bash
+set -e
+
 # DESCRIPTION: This bash script install, and start, a GitHub runner
 #
 # REQUIREMENTS:
 # - GitHub Organization URL (GH_URL)
 # - Token (TOKEN)
 # - Runner Group Name (RUNNER_GRP_NAME)
-# - Runner Name (RUNNER_NAME) [optional]
+# - Runner Name (RUNNER_NAME)
 # - Labels List (LABELS) [optional]
 #
-# USAGE: bash gh-runner-start.sh $GH_URL $TOKEN $RUNNER_GRP_NAME [$RUNNER_NAME] [$LABELS]
+# USAGE: bash gh-runner-start.sh $GH_URL $TOKEN $RUNNER_GRP_NAME $RUNNER_NAME [$LABELS]
 #
-# EXAMPLE: # TODO: to refactor "EXAMPLE"
-# bash ado-agent-start.sh "https://dev.azure.com/fierli92/" "token" "DESKTOP-S8GLSE7 Pool" ["test-agent-name"]
+# EXAMPLE:
+# bash gh-runner-start.sh "https://github.com/Sennar19/new_k8s_kind_from_0" "<TOKEN>" "Docker" "DESKTOP-S8GLSE7" ["DESKTOP-S8GLSE7, docker, ubuntu-24.04"]
 #
 # AUTHORS: Matteo Cristiano
 #
 # VERSION: 1.0.0
 #
-# DATE: 03/04/2025
-
-#!/bin/bash
-set -e
+# DATE: 02/02/2025
 
 ###############################
 ########## Functions ##########
@@ -33,89 +33,112 @@ print_header() {
 
 cleanup() {
   trap "" EXIT
-  if [ -e ./config.sh ]; then
-    print_header "========== Removing agent.. =========="
-    while true; do # Let's check there're no running jobs, before removing the agent
-      ./config.sh remove --unattended --auth "PAT" --token ${ADO_TOKEN} && break # TODO: to manage possible errors, and THEN break the loop
-      echo "There're some running jobs, retrying in 60 seconds.."
-      sleep 60
-    done
-  fi
+#   if [ -e ./config.sh ]; then
+#     print_header "========== Removing agent.. =========="
+#     while true; do # Let's check there're no running jobs, before removing the agent
+#       ./config.sh remove --unattended --auth "PAT" --token ${ADO_TOKEN} && break # TODO: to manage possible errors, and THEN break the loop
+#       echo "There're some running jobs, retrying in 60 seconds.."
+#       sleep 60
+#     done
+#   fi
 }
 
 ############################
 ########## Inputs ##########
 ############################
 
-if [ -z "${ADO_URL}" ]; then
-  echo 1>&2 "ERROR: missing ADO_URL variable"
+GH_URL=$1
+if [ -z $GH_URL ]; then
+  echo 1>&2 "ERROR: missing GH_URL variable"
   exit 1
 fi
 
-if [ -z "${ADO_TOKEN}" ]; then
-  echo 1>&2 "ERROR: missing ADO_TOKEN variable"
+TOKEN=$2
+if [ -z $TOKEN ]; then
+  echo 1>&2 "ERROR: missing TOKEN variable"
   exit 1
 fi
 
-if [ -z "${ADO_POOL}" ]; then
-  echo 1>&2 "ERROR: missing ADO_POOL variable"
+RUNNER_GRP_NAME=$3
+if [ -z $RUNNER_GRP_NAME ]; then
+  echo 1>&2 "ERROR: missing RUNNER_GRP_NAME variable"
   exit 1
 fi
 
-if [ -z "${ADO_AGENT_NAME}" ]; then
-  ADO_AGENT_NAME="$(hostname)_${RANDOM}"
-fi
-
-########################################################
-########## Install Azure DevOps Agent package ##########
-########################################################
-
-print_header "1. Determining package to install.."
-
-AZP_AGENT_PACKAGES=$(curl -LsS -u user:${ADO_TOKEN} -H "Accept:application/json;" "${ADO_URL}/_apis/distributedtask/packages/agent?platform=${TARGETARCH}&top=1")
-if [ "${AZP_AGENT_PACKAGES}" == "The resource cannot be found." ]; then
-  echo 1>&2 "ERROR: could not determine package to install; check that \"${ADO_URL}\" url is valid"
+RUNNER_NAME=$4
+if [ -z $RUNNER_NAME ]; then
+  echo 1>&2 "ERROR: missing RUNNER_NAME variable"
   exit 1
 fi
-if ERROR=$(echo "${AZP_AGENT_PACKAGES}" | jq . 2>&1 >/dev/null); then
-  AZP_AGENT_PACKAGE_LATEST_URL=$(echo "${AZP_AGENT_PACKAGES}" | jq -r ".value[0].downloadUrl")
-else
-    echo "ERROR: Invalid JSON: \"${ERROR}\""
+RUNNER_NAME=$RUNNER_NAME"_$RANDOM"
+echo "Runner \"$RUNNER_NAME\" will be created"
+
+LABELS=$5
+if [ -n "$LABELS" ]; then
+  # Remove spaces around commas and validate the format
+  LABELS=$(echo "$LABELS" | sed 's/ *, */,/g')
+  if ! [[ "$LABELS" =~ ^[a-zA-Z0-9._/-]+(,[a-zA-Z0-9._/-]+)*$ ]]; then
+    echo 1>&2 "ERROR: LABELS must be a comma-separated list of alphanumeric strings, dots, underscores, dashes, or slashes, without spaces"
     exit 1
+  fi
 fi
-if [ -z "${AZP_AGENT_PACKAGE_LATEST_URL}" -o "${AZP_AGENT_PACKAGE_LATEST_URL}" == "null" ]; then
-  echo 1>&2 "ERROR: could not determine package to install; check that \"${ADO_TOKEN}\" token is valid"
+
+#############################################
+########## Create folder structure ##########
+#############################################
+
+print_header "1. Creating folder structure.."
+
+if [ ! -d ~/gh-runners ]; then
+  mkdir -p ~/gh-runners
+fi
+cd ~/gh-runners
+
+if [ ! -d ~/gh-runners/$RUNNER_NAME ]; then
+  mkdir -p ~/gh-runners/$RUNNER_NAME
+fi
+cd ~/gh-runners/$RUNNER_NAME
+
+###################################################
+########## Install GitHub Runner package ##########
+###################################################
+
+print_header "2. Determining, downloading, and extracting package to install.."
+
+GH_RUNNER_PACKAGE=$(curl -s https://api.github.com/repos/actions/runner/releases/latest | jq -r .assets[].browser_download_url | grep "linux-x64")
+if [ -z "$GH_RUNNER_PACKAGE" ]; then
+  echo 1>&2 "ERROR: Could not determine GitHub Runner package to download"
   exit 1
 fi
+GH_RUNNER_VERSION=$(echo "$GH_RUNNER_PACKAGE" | grep -oP 'v\K[0-9]+\.[0-9]+\.[0-9]+')
+echo "Version $GH_RUNNER_VERSION will be installed"
+curl -LsS $(curl -s https://api.github.com/repos/actions/runner/releases/latest | jq -r .assets[].browser_download_url | grep "linux-x64") | tar -xz
+ls -larth
+rm -rf ~/gh-runners/$RUNNER_NAME/*
+ls -larth
 
-echo "DONE!"
+# chmod +x ./run.sh
 
-print_header "2. Downloading, and extracting, package.."
+# source ./env.sh # TODO: to manage possible errors
 
-curl -LsS "${AZP_AGENT_PACKAGE_LATEST_URL}" | tar -xz & wait $! # TODO: to manage possible errors
+# echo "DONE!"
 
-chmod +x ./run.sh
+# print_header "3. Prepare, in case of failure, to remove agent.."
 
-source ./env.sh # TODO: to manage possible errors
+# trap "cleanup; exit 0" EXIT
+# trap "cleanup; exit 130" INT
+# trap "cleanup; exit 143" TERM
 
-echo "DONE!"
+# echo "DONE!"
 
-print_header "3. Prepare, in case of failure, to remove agent.."
+# print_header "4. Configuring agent.."
 
-trap "cleanup; exit 0" EXIT
-trap "cleanup; exit 130" INT
-trap "cleanup; exit 143" TERM
+# ./config.sh --unattended --agent "${ADO_AGENT_NAME}" --url "${ADO_URL}" --auth "PAT" --token "${ADO_TOKEN}" --pool "${ADO_POOL}" --replace --acceptTeeEula & wait $!
 
-echo "DONE!"
+# echo "DONE!"
 
-print_header "4. Configuring agent.."
+# print_header "5. Running agent.."
 
-./config.sh --unattended --agent "${ADO_AGENT_NAME}" --url "${ADO_URL}" --auth "PAT" --token "${ADO_TOKEN}" --pool "${ADO_POOL}" --replace --acceptTeeEula & wait $!
-
-echo "DONE!"
-
-print_header "5. Running agent.."
-
-# To be aware of TERM and INT signals, call "./run.sh" in background, and wait for it
-# TIP: running "./run.sh" with the "--once" flag will shut down the agent, after a single pipeline is executed
-./run.sh "$@" & wait $!
+# # To be aware of TERM and INT signals, call "./run.sh" in background, and wait for it
+# # TIP: running "./run.sh" with the "--once" flag will shut down the agent, after a single pipeline is executed
+# ./run.sh "$@" & wait $!
