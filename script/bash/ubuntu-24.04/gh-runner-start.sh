55e8be1ba54f099ae96e5b46a7409af311704c81
Matteo Cristiano <slb6113@gmail.com>
2025-05-11 22:35:32 +0200

    Update runner configuration in gh-runner-start.sh to use GitHub API for registration token and enable runner service

--- script/bash/ubuntu-24.04/gh-runner-start.sh
+++ script/bash/ubuntu-24.04/gh-runner-start.sh
@@ -112,6 +112,7 @@ chmod +x ~/gh-runners/$RUNNER_NAME/*
 print_header "3. Configuring runner.."
 
 RUNNER_TOKEN=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/create-the-roaring-impossible/actions/runners/registration-token | jq -r .token)
+
 ./config.sh --unattended --name $RUNNER_NAME --url $GH_URL --token $RUNNER_TOKEN --runnergroup $RUNNER_GRP_NAME --replace --labels $LABELS --disableupdate
 
 ########################################################
@@ -120,6 +121,6 @@ RUNNER_TOKEN=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Auth
 
 print_header "4. Running runner.."
 
-# sudo ./svc.sh install
-# sudo ./svc.sh start
-# sudo ./svc.sh status
+sudo ./svc.sh install
+sudo ./svc.sh start
+sudo ./svc.sh status
