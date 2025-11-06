6068650cb4d0df8714edc6113f82135af925a111
Matteo Cristiano <slb6113@gmail.com>
2025-11-06 10:30:06 +0100

    Update Google provider version to 7.10.0 in Terraform configuration

--- terraform/desktop-s8glse7/local/providers.tf
+++ terraform/desktop-s8glse7/local/providers.tf
@@ -14,7 +14,7 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      version = "7.9.0"
+      version = "7.10.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
