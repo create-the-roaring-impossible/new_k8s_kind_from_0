fda295cf0185fc37c00838f005bcd4560a6bad37
Matteo Cristiano <slb6113@gmail.com>
2025-11-17 22:54:39 +0100

    Add local provider configuration to providers.tf

--- terraform/desktop-s8glse7/local/providers.tf
+++ terraform/desktop-s8glse7/local/providers.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.6.1"
+    }
     random = {
       source  = "hashicorp/random"
       version = "3.7.2"
