78192a7cd313c1b244e1bcce75458c68c7340939
Matteo Cristiano <slb6113@gmail.com>
2025-11-21 18:17:53 +0100

    Update content of 'test_1_file' to use heredoc syntax and adjust formatting

--- terraform/desktop-s8glse7/local/main.tf
+++ terraform/desktop-s8glse7/local/main.tf
@@ -25,10 +25,10 @@ resource "local_file" "test_0_file" {
 
 resource "local_file" "test_1_file" {
   filename = "heredoc.txt"
-  content  = <<-EOT
-  This is an example file created by Terraform.
-    test line
-  test line
+  content  = <<-EOF
+This is an example file created by Terraform.
 test line
-  EOT
+test line
+test line
+  EOF
 }
