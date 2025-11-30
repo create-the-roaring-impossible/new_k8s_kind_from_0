2b3512177e36a949c765a0cd6c073b17e870157e
Matteo Cristiano <slb6113@gmail.com>
2025-11-30 14:58:08 +0100

    Fix output definition for random_integer in outputs.tf to reference the correct result

--- terraform/desktop-s8glse7/local/outputs.tf
+++ terraform/desktop-s8glse7/local/outputs.tf
@@ -1,7 +1,7 @@
-# output "random_integer" {
-#   value = random_integer.rand.result
-# }
-
 output "random_integer" {
-  value = random_integer.rand_new.result
+  value = random_integer.rand.result
 }
+
+# output "random_integer" {
+#   value = random_integer.rand_new.result
+# }
