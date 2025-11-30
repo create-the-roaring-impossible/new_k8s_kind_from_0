3f9729519bd2001bde47e99a5541e5ae14c6bb8a
Matteo Cristiano <slb6113@gmail.com>
2025-12-01 00:46:02 +0100

    Refactor local_file resources in main.tf to update file movement and organization

--- terraform/desktop-s8glse7/local/main.tf
+++ terraform/desktop-s8glse7/local/main.tf
@@ -3,20 +3,20 @@ resource "random_integer" "rand" {
   max = 100
 }
 
-resource "local_file" "test_file" { # commented to test "state mv" command
-  filename = "${random_integer.rand.result}.txt"
-  content  = "This is an example file created by Terraform."
-}
-
-# moved {
-#   from = random_integer.rand
-#   to  = random_integer.rand_new
-# }
-
-# resource "local_file" "test_file_new" {
+# resource "local_file" "test_file" { # commented to test "state mv" command
 #   filename = "${random_integer.rand.result}.txt"
 #   content  = "This is an example file created by Terraform."
 # }
+
+moved {
+  from = local_file.test_file
+  to  = local_file.test_file_new
+}
+
+resource "local_file" "test_file_new" {
+  filename = "${random_integer.rand.result}.txt"
+  content  = "This is an example file created by Terraform."
+}
 
 resource "local_file" "test_file_heredoc" {
   filename = "heredoc.txt"
