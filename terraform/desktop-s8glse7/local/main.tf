63780580280c167db8191fc11d70c36c9c89c890
Matteo Cristiano <slb6113@gmail.com>
2025-11-30 14:54:08 +0100

    Format and uncomment resource definitions for random_integer and local_file in main.tf

--- terraform/desktop-s8glse7/local/main.tf
+++ terraform/desktop-s8glse7/local/main.tf
@@ -1,34 +1,34 @@
-# resource "random_integer" "rand" {
-#   min = 1
-#   max = 100
-# }
+resource "random_integer" "rand" {
+  min = 1
+  max = 100
+}
+
+resource "local_file" "test_file" {
+  filename = "${random_integer.rand.result}.txt"
+  content  = "This is an example file created by Terraform."
+}
 
 # moved {
 #   from = random_integer.rand
 #   to  = random_integer.rand_new
 # }
 
-resource "random_integer" "rand_new" {
-  min = 1
-  max = 100
-}
+# resource "random_integer" "rand_new" {
+#   min = 1
+#   max = 100
+# }
 
-# resource "local_file" "test_file" {
-#   filename = "${random_integer.rand.result}.txt"
+# resource "local_file" "test_0_file" {
+#   filename = "${random_integer.rand_new.result}.txt"
 #   content  = "This is an example file created by Terraform."
 # }
 
-resource "local_file" "test_0_file" {
-  filename = "${random_integer.rand_new.result}.txt"
-  content  = "This is an example file created by Terraform."
-}
-
-resource "local_file" "test_1_file" {
-  filename = "heredoc.txt"
-  content  = <<-EOF
-This is an example file created by Terraform.
-test line
-test line
-test line
-  EOF
-}
+# resource "local_file" "test_1_file" {
+#   filename = "heredoc.txt"
+#   content  = <<-EOF
+# This is an example file created by Terraform.
+# test line
+# test line
+# test line
+#   EOF
+# }
