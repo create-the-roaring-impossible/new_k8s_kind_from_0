7befaf65e21aa677b36296a1e1c5ad343cc2e2e2
Matteo Cristiano <slb6113@gmail.com>
2025-12-01 11:24:43 +0100

    Restore local_file resource in main.tf for state mv testing

--- terraform/desktop-s8glse7/local/main.tf
+++ terraform/desktop-s8glse7/local/main.tf
@@ -3,15 +3,15 @@ resource "random_integer" "rand" {
   max = 100
 }
 
-# resource "local_file" "test_file" { # commented to test "state mv" command
-#   filename = "${random_integer.rand.result}.txt"
-#   content  = "This is an example file created by Terraform."
-# }
-
-moved { # comment "local_file.test_file" and "local_file.test_file_new", before to use it
-  from = local_file.test_file
-  to  = local_file.test_file_new
+resource "local_file" "test_file" { # commented to test "state mv" command
+  filename = "${random_integer.rand.result}.txt"
+  content  = "This is an example file created by Terraform."
 }
+
+# moved { # comment "local_file.test_file" and "local_file.test_file_new", before to use it
+#   from = local_file.test_file
+#   to  = local_file.test_file_new
+# }
 
 # resource "local_file" "test_file_new" {
 #   filename = "${random_integer.rand.result}.txt"
