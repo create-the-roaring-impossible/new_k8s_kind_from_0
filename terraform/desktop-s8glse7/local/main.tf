dde878b0fa64f1c0069c53b232b2d744269fcd54
Matteo Cristiano <slb6113@gmail.com>
2025-12-01 11:40:45 +0100

    Refactor local_file resource handling for state mv testing

--- terraform/desktop-s8glse7/local/main.tf
+++ terraform/desktop-s8glse7/local/main.tf
@@ -3,20 +3,20 @@ resource "random_integer" "rand" {
   max = 100
 }
 
-resource "local_file" "test_file" { # commented to test "state mv" command
-  filename = "${random_integer.rand.result}.txt"
-  content  = "This is an example file created by Terraform."
-}
+# resource "local_file" "test_file" { # commented before to use "moved" block
+#   filename = "${random_integer.rand.result}.txt"
+#   content  = "This is an example file created by Terraform."
+# }
 
-moved { # comment "local_file.test_file" and "local_file.test_file_new", before to use it
+moved { # comment "local_file.test_file" and decomment "local_file.test_file_new", before to use it
   from = local_file.test_file
   to  = local_file.test_file_new
 }
 
-# resource "local_file" "test_file_new" {
-#   filename = "${random_integer.rand.result}.txt"
-#   content  = "This is an example file created by Terraform."
-# }
+resource "local_file" "test_file_new" { # decommented before to use "moved" block
+  filename = "${random_integer.rand.result}.txt"
+  content  = "This is an example file created by Terraform."
+}
 
 resource "local_file" "test_file_heredoc" {
   filename = "heredoc.txt"
