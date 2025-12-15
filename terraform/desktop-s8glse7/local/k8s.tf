8519783c52b2e434806d95db5888463cb131330a
Matteo Cristiano <slb6113@gmail.com>
2025-12-15 23:37:39 +0100

    Add Kubernetes namespace resource definition in k8s.tf

--- terraform/desktop-s8glse7/local/k8s.tf
+++ terraform/desktop-s8glse7/local/k8s.tf
@@ -1,1 +1,12 @@
-# test
+resource "kubernetes_namespace" "gh_ns" {
+  metadata {
+    annotations = {
+      name = "gh-annotation"
+    }
+    labels = {
+      mylabel = "gh-label"
+    }
+    name = "gh-ns"
+  }
+  wait_for_default_service_account = true
+}
