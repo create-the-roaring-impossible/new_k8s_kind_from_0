afca17d3baea9cc46b9da7b69563f4f994337865
Matteo Cristiano <slb6113@gmail.com>
2026-01-25 23:02:10 +0100

    Refactor lifecycle precondition in Kubernetes namespace resource to enforce 'gh' as the only valid scope

--- terraform/desktop-s8glse7/local/k8s.tf
+++ terraform/desktop-s8glse7/local/k8s.tf
@@ -13,10 +13,10 @@ resource "kubernetes_namespace" "ns" {
     name = "${var.scope}-ns"
   }
   wait_for_default_service_account = true
-  # lifecycle {
-  #   precondition {
-  #     condition = asdasdasd # TODO: to set a condition on "scope" as "ado"
-  #     error_message = "Error: you can only set '${var.scope}' as scope"
-  #   }
-  # }
+  lifecycle {
+    precondition {
+      condition = ${var.scope} == "gh"
+      error_message = "Error: you can only set 'gh' as scope"
+    }
+  }
 }
